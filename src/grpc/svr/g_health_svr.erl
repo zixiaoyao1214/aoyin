b9c38377220eea350d559c309b11f3d7b16f7b82
yr <1207595963@qq.com>
2021-01-19 20:08:10 +0800

    fix

--- src/grpc/svr/g_health_svr.erl
+++ src/grpc/svr/g_health_svr.erl
@@ -9,23 +9,14 @@
 -module(g_health_svr).
 -author("yr").
 
--behavior(grpcbox_health_bhvr).
-
 -export([check/2,
     watch/2]).
 
-%% @doc Unary RPC
--callback check(ctx:ctx(), grpcbox_health_pb:health_check_request()) ->
-    {ok, grpcbox_health_pb:health_check_response(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
 check(Ctx, #{service := <<>>}) ->
     {ok, #{status => 'SERVING'}, Ctx};
 check(Ctx, #{service := _Service}) ->
     %% TODO: lookup if we are serving this service
     {ok, #{status => 'UNKNOWN'}, Ctx}.
 
-
-%% @doc
--callback watch(grpcbox_health_pb:health_check_request(), grpcbox_stream:t()) ->
-    ok | grpcbox_stream:grpc_error_response().
 watch(_Request, _Stream) ->
     ok.
