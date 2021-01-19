4e57054048a28161192e6f969f51b35572ca24d6
yr <1207595963@qq.com>
2021-01-19 17:20:01 +0800

    add server

---
+++ src/grpc/svr/g_return_svr.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% @author yr
+%%% @copyright (C) 2021, <yanFan>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. 1月 2021 18:23
+%%%-------------------------------------------------------------------
+-module(g_return_svr).
+-author("yr").
+
+-behavior(grpc_return_bhvr).
+
+%% API
+-export([get_money/2]).
+
+-callback get_money(ctx:ctx(), ct_return_pb:account_plat()) ->
+    {ok, ct_return_pb:charge(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
+get_money(CTX, #{account := Account, platform := Platform}) ->
+    Money1 = case return_charge:get_account_money({Account, Platform}) of
+                 Money when Money > 0 ->
+                     Money;
+                 _ ->
+                     0
+             end,
+    Reply = #{money => Money1},
+    {ok, Reply, CTX}.
