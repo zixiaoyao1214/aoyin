1058dc6dbe29db2248153beb6f5305ecd222213b
yr <1207595963@qq.com>
2021-01-19 20:28:35 +0800

    fix

--- src/aoyin_mgr.erl
+++ src/aoyin_mgr.erl
@@ -8,7 +8,11 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([
+    start_link/0
+    , monitor/2
+]).
+
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
     code_change/3]).
 
@@ -35,7 +39,7 @@ handle_cast(_Request, State = #aoyin_mgr_state{}) ->
     {noreply, State}.
 
 handle_info({monitor, Name}, State = #aoyin_mgr_state{}) ->
-    monitor(Name),
+    spawn(?MODULE, monitor, [Name, self()]),
     {noreply, State};
 handle_info(_Info, State = #aoyin_mgr_state{}) ->
     {noreply, State}.
@@ -53,17 +57,17 @@ code_change(_OldVsn, State = #aoyin_mgr_state{}, _Extra) ->
 monitor() ->
     case application:get_env(grpcbox, client) of
         {ok, #{channels := Channels}} ->
-            [monitor(Name)
+            [spawn(?MODULE, monitor, [Name, self()])
                 || {Name, _Endpoints, _Options} <- Channels];
         _ ->
             ok
     end.
 
-monitor(Name) ->
+monitor(Name, PID) when is_pid(PID) ->
     case grpcbox_health_client:check(#{service => <<>>}, #{channel => Name}) of
         {ok, #{status := 'SERVING'}, _} ->
             ets:insert(aoyin_client_info, {Name});
         _ ->
             ets:delete(aoyin_client_info, Name)
     end,
-    erlang:send_after(5000, self(), {monitor, Name}).
+    erlang:send_after(5000, PID, {monitor, Name}).
